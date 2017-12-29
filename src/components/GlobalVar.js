a256b38f8d582945348e9180bfd8b5911b2cb6c0
Lennert Van sever <lennert_vansever@hotmail.be>
2017-12-29 15:53:21 +0100

    method to close browser from messenger

--- src/components/GlobalVar.js
+++ src/components/GlobalVar.js
@@ -1,14 +1,26 @@
 class GlobalVar{
-    getServerLink(){
-      const thisHostName = window.location.hostname;
-      console.log(thisHostName);
-      if(thisHostName === "localhost"){
-          return "http://localhost:5000";
-      }
-      else if(thisHostName === "government-client.herokuapp.com"){
-          return 'https://government-in-the-pocket.herokuapp.com';
-      }
+  getServerLink(){
+    const thisHostName = window.location.hostname;
+    console.log(thisHostName);
+    if(thisHostName === "localhost"){
+        return "http://localhost:5000";
     }
+    else if(thisHostName === "government-client.herokuapp.com"){
+        return 'https://government-in-the-pocket.herokuapp.com';
+    }
+  }
+  closeBrowser(){
+    const script = document.createElement("script");
+    const scriptText = document.createTextNode(`
+      MessengerExtensions.requestCloseBrowser(function success() {
+        console.log("webview closed");
+      }, function error(err) {
+        console.log("error ocured", err);
+      });
+    `);
+    script.appendChild(scriptText);
+    document.head.appendChild(script);
+  }
 }
 
 export default (new GlobalVar());
