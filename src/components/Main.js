94d0053af79575acb5c5ba2e9d0a58a1793898ad
Lennert Van sever <lennert_vansever@hotmail.be>
2017-12-29 15:52:50 +0100

    setting up messenger sdk

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,19 @@ import '../style/css/main.css';
 import Login from './forms/Login';
 
 class Main extends Component {
-    render() {
-    	return (
-    		<div className="content">
-    			<Route path="/forms/login/:facebook_id" component={Login} />
-    		</div>
-		  );
-    }
+  componentWillMount(){
+    const script = document.createElement("script");
+    const scriptText = document.createTextNode(`(function(d, s, id){var js, fjs=d.getElementsByTagName(s)[0]; if (d.getElementById(id)){return;}js=d.createElement(s); js.id=id; js.src="//connect.facebook.com/en_US/messenger.Extensions.js"; fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'Messenger'));`);
+    script.appendChild(scriptText);
+    document.head.appendChild(script);
+  }
+  render() {
+  	return (
+  		<div className="content">
+  			<Route path="/forms/login/:facebook_id" component={Login} />
+  		</div>
+	  );
+  }
 }
 
 export default Main;
