eb845ad8d5961577f3c3c420de050e0ebb562a3c
Lennert Van sever <lennert_vansever@hotmail.be>
2017-12-29 13:17:21 +0100

    generating correcting live server link

--- src/components/GlobalVar.js
+++ src/components/GlobalVar.js
@@ -1,12 +1,13 @@
 class GlobalVar{
     getServerLink(){
-        const thisHostName = window.location.hostname;
-        if(thisHostName === "localhost"){
-            return "http://localhost:5000";
-        }
-        else if(thisHostName === "government-in-the-pocket.herokuapp.com"){
-            return 'https://government-in-the-pocket.herokuapp.com';
-        }
+      const thisHostName = window.location.hostname;
+      console.log(thisHostName);
+      if(thisHostName === "localhost"){
+          return "http://localhost:5000";
+      }
+      else if(thisHostName === "government-client.herokuapp.com"){
+          return 'https://government-in-the-pocket.herokuapp.com';
+      }
     }
 }
 
